9b360eed8191646c43a9f257fd1ec565230a3dbf
sushmamylavarapu <128815530+sushmamylavarapu@users.noreply.github.com>
2023-06-14 03:40:10 -0400

    Pushing final updated files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "09453945f309bbd6eb6cd5a2109093b2"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "f8d3b64abb8345ed9f71408e78f383fa"
